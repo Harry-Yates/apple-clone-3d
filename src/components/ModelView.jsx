41d77fae0beb4744d6694aad3253476004616e2b
Harr-Yates <hyates1@gmail.com>
2024-03-24 13:53:40 +0100

    loaded iphone model

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,5 +1,39 @@
-const ModelView = () => {
-  return <div>ModelView</div>;
+import { PerspectiveCamera, View } from "@react-three/drei";
+import Lights from "./Lights";
+import { Suspense } from "react";
+import IPhone from "./IPhone";
+
+const ModelView = ({
+  index,
+  groupRef,
+  gsapType,
+  controlRef,
+  setRotationSize,
+  size,
+  item,
+}) => {
+  return (
+    <View
+      index={index}
+      id={gsapType}
+      className={`border-2 border-red-500 w-full h-full ${
+        index === 2
+      } ? 'right-[100%] : ''`}>
+      {/* Ambient Light */}
+      <ambientLight intensity={0.3} />
+
+      <PerspectiveCamera
+        makeDefault
+        position={[0, 0, 4]}
+        ref={controlRef}
+      />
+
+      <Lights />
+      <Suspense fallback={<div>Loading</div>}>
+        <IPhone />
+      </Suspense>
+    </View>
+  );
 };
 
 export default ModelView;
